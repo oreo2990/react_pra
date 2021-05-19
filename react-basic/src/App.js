df19ecf0b154e36d2ed761589090d3fc8ecad0e4
oreo2990 <74302915+oreo2990@users.noreply.github.com>
2021-05-19 17:18:05 +0900

    コンポーネントの練習です

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Article from "./components/Article";
+
 
 function App() {
   return (
@@ -12,6 +14,12 @@ function App() {
         <p>
           新・日本一わかりやすいReact入門シリーズで環境構築しました。
         </p>
+
+        <Article 
+          tittle="コンポーネントの練習"
+          content="コンポーネントは便利"
+        />
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ react-basic/src/components/Article.jsx
@@ -0,0 +1,11 @@
+
+const Article = (props) => {
+    return(
+        <div>
+            <p>{props.tittle}</p>
+            <p>{props.content}</p>
+        </div>
+    )
+}
+
+export default Article
